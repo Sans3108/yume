91183c726759319a93d5ae3bc9c4053c48e14201
Glitch (sans3108-yume) <none>
2020-01-28 11:12:11 +0000

    🚘☔️ Updated with Glitch

--- commands/ping.js
+++ commands/ping.js
@@ -1,20 +1,22 @@
 const Discord = require("discord.js");
 
 module.exports = {
-    name: 'ping',
-    description: 'PONG!',
-    cooldown: 2,
-    execute(message, args, bot, color) {
-        const embed1 = new Discord.RichEmbed()
-        .setColor(color)
-        .setDescription('Pinging...');
+  name: "ping",
+  description: "PONG!",
+  cooldown: 2,
+  execute(message, args, bot, color) {
+    const embed1 = new Discord.RichEmbed()
+      .setColor(color)
+      .setDescription("Pinging...");
 
-        message.channel.send(embed1).then(sent => {
-            const embed1Edit = new Discord.RichEmbed()
-            .setColor(color)
-            .setDescription(`Pong! :ping_pong:\nIt took me ${sent.createdTimestamp - message.createdTimestamp}ms`);
-            
-            sent.edit(embed1Edit)
-        });
-    },
+    message.channel.send(embed1).then(sent => {
+      const embed1Edit = new Discord.RichEmbed()
+        .setColor(color)
+        .setDescription(
+          `Pong! :ping_pong:\n\n:person_running: Response time: ${sent.createdTimestamp - message.createdTimestamp}ms\n:clock4: Ping: ${bot.ping}ms`
+        );
+
+      sent.edit(embed1Edit);
+    });
+  }
 };
